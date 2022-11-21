d17d6f4e1b0e9e5eb7444e9e63124fd66f6398a4
alexanderjsn <alexanderjsn@live.com>
2022-11-21 14:26:03 +0000

    fixed typing errors in print_board function

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
-import random
+
 
 player_board = []
 
-for _ in range (5):
-    player_board.append(["O", ] * 5)
+for _ in range(5):
+    player_board.append(["O"] * 5)
 
-def print_board(player_board):
-    for row in player_board:
-        print((" ". join(row)))
-print("name's board")
+    def print_board(player_board):
+        for row in player_board:
+            print((" ". join(row)))
 print_board(player_board)
