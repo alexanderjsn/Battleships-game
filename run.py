c10e41c6bd79e969b3d1efd4d19e74d406a68b27
alexanderjsn <alexanderjsn@live.com>
2022-11-21 19:35:39 +0000

    added rules for winning and losing to game

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def print_board(player_board):
 print("User board")
 print_board(player_board)
 
-print("_" * 20)
+print(" ")
 
 
 def print_comp_board(computer_board):
@@ -32,12 +32,10 @@ for ships in range(1):
     player_col = int(input("Enter a column to place ship (0-4): "))
     player_board[player_row][player_col] = "@"
     print_board(player_board)
-    print("_" * 20)
+    comp_row = random.randint(1, 4)
+    comp_col = random.randint(1, 4)
+    print(" ")
     print_comp_board(computer_board)
-
-
-comp_row = random.randint(1, 4)
-comp_col = random.randint(1, 4)
 
 
 def guess_computer_ship(computer_board):
@@ -46,13 +44,24 @@ def guess_computer_ship(computer_board):
         col_guess = int(input("Guess a column: "))
         computer_board[row_guess][col_guess] = "X"
         print_board(player_board)       
-        print("_" * 20)
+        print(" ")
         print_comp_board(computer_board)
         comp_row_guess = random.randint(1, 4)
         comp_col_guess = random.randint(1, 4)
-        player_board[comp_row_guess][comp_col_guess] = "X"
         print("Computer guessed column: ", comp_col_guess)
         print("Computer guessed row: ", comp_row_guess)
+        player_board[comp_row_guess][comp_col_guess] = "X"
+        if row_guess == comp_row and col_guess == comp_col:
+            print("You sunk my battleship. Good job!")
+            break
+        else:
+            if row_guess < 0 or row_guess > 4 or col_guess < 0 or col_guess > 4:
+                print("You're off the grid, need to be a number between 0-4")
+            elif computer_board[row_guess][col_guess] == "X":
+                print("You missed!")
+            elif comp_row_guess == player_row and comp_col_guess == player_col:
+                print("I sunk your battleship. Loser!")
+                break
 
 
 guess_computer_ship(computer_board)
